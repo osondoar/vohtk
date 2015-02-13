c223d412802ab4a6ca8438fdcd0f89d56345374f
Rafael García Pardo <r@ragar.me>
2015-02-13 00:02:09 -0600

    Add custom logger

--- app.go
+++ app.go
@@ -12,13 +12,18 @@ import (
 	"github.com/osondoar/vohtk/errors"
 )
 
+var (
+	logger, logFile = InitLogger()
+)
+
 func main() {
 	var mainController controllers.MainController
 	var requestsController api_controllers.RequestsController
-
+	defer logFile.Close()
 	InitDirectories()
+
 	r := mux.NewRouter()
-	r.HandleFunc("/", mainController.Index)
+	r.Handle("/", htmlHandler(mainController.Index))
 	r.Handle("/api/requests", apiHandler(requestsController.Post)).Methods("POST")
 
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./assets/")))
@@ -30,6 +35,7 @@ func main() {
 
 // http://blog.golang.org/error-handling-and-go
 type apiHandler func(http.ResponseWriter, *http.Request) *errors.AppError
+type htmlHandler func(http.ResponseWriter, *http.Request)
 
 func (fn apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if e := fn(w, r); e != nil { // e is *appError, not os.Error.
@@ -40,6 +46,11 @@ func (fn apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (fn htmlHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	logger.Printf("%q %q Referer: %q", r.UserAgent(), r.RemoteAddr, r.Referer())
+	fn(w, r)
+}
+
 func InitDirectories() {
 	err1 := os.MkdirAll("tmp/wav", 0775)
 	err2 := os.MkdirAll("tmp/mfc", 0775)
@@ -48,4 +59,14 @@ func InitDirectories() {
 	if err1 != nil || err2 != nil || err3 != nil {
 		log.Printf("Error creating directories: %q, %q, %q", err1, err2, err3)
 	}
+}
+
+func InitLogger() (*log.Logger, *os.File) {
+	f, err := os.OpenFile("logs/access.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal("error opening file: %q", err)
+	}
+	// defer f.Close()
+	logger := log.New(f, "", log.Ldate|log.Ltime)
+	return logger, f
 }
